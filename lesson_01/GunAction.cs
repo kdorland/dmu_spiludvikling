a13fc8221a851eafdc8020c220cff2bf3dddb864
Kristian Dorland <kristian@dorland.dk>
2018-08-13 08:30:48 +0200

    Initial commit, lesson 1

---
+++ lesson_01/GunAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GunAction : MonoBehaviour
+{
+    public Rigidbody bullet;
+    public Transform bulletPoint;
+    public float bulletSpeed;
+
+    public void Update()
+    {
+        // Check for Fire1 input and instantiate bullet prefab at bulletPoint         
+        if ((Input.GetButtonDown("Fire1")))
+        {
+            Rigidbody bulletInstance = Instantiate(bullet, bulletPoint.transform.position, bulletPoint.transform.rotation);
+
+            // The local up direction of the gun is transformed into world space.
+            Vector3 gunDirection = transform.TransformDirection(Vector3.up);
+            bulletInstance.AddForce(gunDirection * bulletSpeed, ForceMode.Impulse);
+        }
+    }
+}
---
+++ lesson_01/MoveTarget.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    private Vector3 targetPoint;
+    public float speed = 1;
+    public int pointsIfHit;
+    public ScoreController controller;
+
+    private void Start()
+    {
+        targetPoint = pointA.position;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        controller.AddScore(pointsIfHit);
+        Destroy(gameObject);
+    }
+
+    // Move target between targets
+    public void Update() {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            targetPoint, 
+            Time.deltaTime * speed);
+
+        if (transform.position == targetPoint) {
+            if (targetPoint == pointA.position) {
+                targetPoint = pointB.position;
+            } else {
+                targetPoint = pointA.position;
+            }
+        }
+    }
+}
